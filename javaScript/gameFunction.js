3c75db1f1e58f0e3961061220eae836c8a935c58
mollybuhl <molly.buhl@telia.com>
2024-04-11 18:31:36 +0200

    Functions for different control questions per charracter

--- javaScript/gameFunction.js
+++ javaScript/gameFunction.js
@@ -32,15 +32,39 @@ function startTimer(){
 
 function renderMap(){
 
+    //Start game timer
     startTimer();
 
+    //Map structure
     main.classList.add("mainMap");
     main.innerHTML=`
-    <button class="logOutBtn">Log out</button>
-    <div class="pin hoock"></div>
-    <div class="pin cruella"></div>
-    <div class="pin princeJohn"></div>
-    <div class="pin ursulla"></div>
+    <button class="logOutBtn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
+        <mask id="mask0_108_37" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="35" height="35">
+        <rect width="35" height="35" fill="#D9D9D9"/>
+        </mask>
+        <g mask="url(#mask0_108_37)">
+        <path d="M17.6667 17.5C16.0625 17.5 14.6892 16.9288 13.5469 15.7864C12.4045 14.6441 11.8333 13.2708 11.8333 11.6666C11.8333 10.0625 12.4045 8.68922 13.5469 7.54685C14.6892 6.40449 16.0625 5.83331 17.6667 5.83331C19.2708 5.83331 20.6441 6.40449 21.7865 7.54685C22.9288 8.68922 23.5 10.0625 23.5 11.6666C23.5 13.2708 22.9288 14.6441 21.7865 15.7864C20.6441 16.9288 19.2708 17.5 17.6667 17.5ZM6 29.1666V25.0833C6 24.2569 6.21267 23.4974 6.63802 22.8047C7.06337 22.112 7.62847 21.5833 8.33333 21.2187C9.84028 20.4653 11.3715 19.9002 12.9271 19.5234C14.4826 19.1467 16.0625 18.9583 17.6667 18.9583C19.2708 18.9583 20.8507 19.1467 22.4062 19.5234C23.9618 19.9002 25.4931 20.4653 27 21.2187C27.7049 21.5833 28.27 22.112 28.6953 22.8047C29.1207 23.4974 29.3333 24.2569 29.3333 25.0833V29.1666H6ZM8.91667 26.25H26.4167V25.0833C26.4167 24.816 26.3498 24.5729 26.2161 24.3541C26.0825 24.1354 25.9062 23.9653 25.6875 23.8437C24.375 23.1875 23.0503 22.6953 21.7135 22.3672C20.3767 22.039 19.0278 21.875 17.6667 21.875C16.3056 21.875 14.9566 22.039 13.6198 22.3672C12.283 22.6953 10.9583 23.1875 9.64583 23.8437C9.42708 23.9653 9.25087 24.1354 9.11719 24.3541C8.98351 24.5729 8.91667 24.816 8.91667 25.0833V26.25ZM17.6667 14.5833C18.4688 14.5833 19.1554 14.2977 19.7266 13.7265C20.2977 13.1554 20.5833 12.4687 20.5833 11.6666C20.5833 10.8646 20.2977 10.1779 19.7266 9.60675C19.1554 9.03557 18.4688 8.74998 17.6667 8.74998C16.8646 8.74998 16.178 9.03557 15.6068 9.60675C15.0356 10.1779 14.75 10.8646 14.75 11.6666C14.75 12.4687 15.0356 13.1554 15.6068 13.7265C16.178 14.2977 16.8646 14.5833 17.6667 14.5833Z" fill="#1C1B1F" fill-opacity="0.47"/>
+        </g>
+        </svg>
+    </button>
+    
+   
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="43" viewBox="0 0 32 43" fill="none" class="pin hoock">
+    <path d="M32 16.125C32 23.4652 22.25 36.5332 17.975 41.925C16.95 43.21 15.05 43.21 14.025 41.925C9.75 36.5332 0 23.4652 0 16.125C0 7.22266 7.16667 0 16 0C24.8333 0 32 7.22266 32 16.125Z" fill="black"/>
+    </svg>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="43" viewBox="0 0 32 43" fill="none" class="pin cruella">
+    <path d="M32 16.125C32 23.4652 22.25 36.5332 17.975 41.925C16.95 43.21 15.05 43.21 14.025 41.925C9.75 36.5332 0 23.4652 0 16.125C0 7.22266 7.16667 0 16 0C24.8333 0 32 7.22266 32 16.125Z" fill="black"/>
+    </svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="43" viewBox="0 0 32 43" fill="none" class="pin musse">
+    <path d="M32 16.125C32 23.4652 22.25 36.5332 17.975 41.925C16.95 43.21 15.05 43.21 14.025 41.925C9.75 36.5332 0 23.4652 0 16.125C0 7.22266 7.16667 0 16 0C24.8333 0 32 7.22266 32 16.125Z" fill="black"/>
+    </svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="43" viewBox="0 0 32 43" fill="none" class="pin ursulla">
+    <path d="M32 16.125C32 23.4652 22.25 36.5332 17.975 41.925C16.95 43.21 15.05 43.21 14.025 41.925C9.75 36.5332 0 23.4652 0 16.125C0 7.22266 7.16667 0 16 0C24.8333 0 32 7.22266 32 16.125Z" fill="black"/>
+    </svg>
 
     <div class="navbar">
         <div class="charracterChart">
@@ -76,16 +100,16 @@ function renderMap(){
     </div>
     `;
 
-    main.querySelector(".leaderboard").addEventListener("click", renderLeaderboard);
-    main.querySelector(".charracterChart").addEventListener("click", renderCharracterboard);
-
+    //When selecting a pin, display charracter pop up
     main.querySelectorAll(".pin").forEach(pin =>
-
         pin.addEventListener("click", e => {
 
+            //Hide navbar
             main.querySelector(".navbar").classList.add("hidden");
 
+            //If pin is already selected unselect, otherwise display charracter pop up
             if(pin.classList.contains("selected")){
+
                 pin.classList.remove("selected");
                 document.querySelector(".popUp").remove();
                 main.querySelector(".navbar").classList.remove("hidden");
@@ -103,7 +127,7 @@ function renderMap(){
                 let classList = pin.classList;
                 let selectedCharracter;
                
-    
+                //Find selected charracter
                 for (let i = 0; i < classList.length; i++) {
                     switch (classList[i]) {
                         case "hoock":
@@ -112,8 +136,8 @@ function renderMap(){
                         case "cruella":
                             selectedCharracter = "Cruella";
                             break;
-                        case "princeJohn":
-                            selectedCharracter = "Prince John";
+                        case "musse":
+                            selectedCharracter = "Musse";
                             break;
                         case "ursulla":
                             selectedCharracter = "Ursulla";
@@ -136,30 +160,28 @@ function renderMap(){
                 `;
     
                 main.appendChild(popUp);
+
+                //Display controll question when clicking "talk to charracter"
                 document.querySelector(".btnTalkToCharracter").addEventListener("click", e =>{
                     
-                    main.classList.remove("mainMap");
-                    main.innerHTML = `
-                    <h2>Vilken färg är dörren? (blå)</h2>
-                    <input>Svar</input>
-                    <button class="btnUnlock">Lås upp</button>
-                    `;
-                
-                    let input = main.querySelector("input");
+                    //Check if controll questio has already been answered 
 
-                    main.querySelector(".btnUnlock").addEventListener("click", e =>{
-                        let answer = input.value;
-                        if(answer === "blå"){
-                            renderCharracterPage(selectedCharracter)
-                        }else{
-                            console.log("Wrong answer");
-                        }
-                    })
+                    //If controll question has not been answered before
+                    renderControlQuestion(selectedCharracter);
+
+                    //If controll question has already been answered before
+
+                    
                 })
             }
         })    
     )
 
+    //Navbar
+    main.querySelector(".leaderboard").addEventListener("click", renderLeaderboard);
+    main.querySelector(".charracterChart").addEventListener("click", renderCharracterboard);
+
+    // Logout
     main.querySelector(".logOutBtn").addEventListener("click", e => {
         window.localStorage.setItem("loggedIn", "false");
         window.localStorage.removeItem("userId");
@@ -167,20 +189,130 @@ function renderMap(){
     })
 }
 
+//Function to render controll questions before player get the information from a charracter
+function renderControlQuestion(charracter){
+
+    main.classList.remove("mainMap");
+
+    let typeOfQuestion;
+    let question;
+    let answer;
+
+    switch (charracter) {
+        case "Hoock":
+            typeOfQuestion = "word";
+            question = "Vilket namn har det mystiska djuret med en sydvästlig riktning? (pegasus)";
+            answer = "pegasus";
+            break;
+        case "Cruella":
+            typeOfQuestion = "crossword";
+            question = ["bro","bänk","soptunna"]
+            answer = "blå";
+            break;
+        case "Musse":
+            typeOfQuestion = "number";
+            question = "Hur många x kan du se här inne på gården? (9)";
+            answer = "9";
+            break;
+        case "Ursulla":
+            typeOfQuestion = "word";
+            question = "Vilken färg har dörren? (blå)";
+            answer = "blå";
+            break;
+    }
+
+    if(typeOfQuestion === "number"){
+
+        main.innerHTML = `
+        <h2>${question}</h2>
+        <input type="number">Svar</input>
+        <button class="btnUnlock">Lås upp</button>
+        `;
+    }else if(typeOfQuestion === "word"){
+        main.innerHTML = `
+        <h2>${question}</h2>
+        <input>Svar</input>
+        <button class="btnUnlock">Lås upp</button>
+        `;
+    }else if(typeOfQuestion === "crossword"){
+        main.innerHTML = `
+        <h2>${question}</h2>
+        
+        <button class="btnUnlock">Lås upp</button>
+        `;
+    }
+   
+                
+    let input = main.querySelector("input");
+
+    main.querySelector(".btnUnlock").addEventListener("click", e =>{
+        let userAnswer = input.value;
+        console.log(userAnswer, answer);
+        if(userAnswer === answer){
+            renderCharracterPage(charracter)
+        }else{
+            console.log("Wrong answer");
+        }
+    })
+
+}
+
 
 function renderCharracterPage(charracter){
+
+    let charracterText;
+    //Define carracter text based on charracter selected
+    switch (charracter) {
+        case "Hoock":
+            charracterText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Vitae tortor condimentum lacinia quis vel eros donec. Tristique et egestas quis ipsum. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Curabitur vitae nunc sed velit dignissim sodales ut. Risus nullam eget felis eget nunc lobortis mattis aliquam. Venenatis a condimentum vitae sapien pellentesque. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. ";
+            break;
+        case "Cruella":
+            charracterText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Vitae tortor condimentum lacinia quis vel eros donec. Tristique et egestas quis ipsum. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Curabitur vitae nunc sed velit dignissim sodales ut. Risus nullam eget felis eget nunc lobortis mattis aliquam. Venenatis a condimentum vitae sapien pellentesque. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. ";
+            break;
+        case "Musse":
+            charracterText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Vitae tortor condimentum lacinia quis vel eros donec. Tristique et egestas quis ipsum. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Curabitur vitae nunc sed velit dignissim sodales ut. Risus nullam eget felis eget nunc lobortis mattis aliquam. Venenatis a condimentum vitae sapien pellentesque. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. ";
+            break;
+        case "Ursulla":
+            charracterText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Vitae tortor condimentum lacinia quis vel eros donec. Tristique et egestas quis ipsum. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Curabitur vitae nunc sed velit dignissim sodales ut. Risus nullam eget felis eget nunc lobortis mattis aliquam. Venenatis a condimentum vitae sapien pellentesque. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. ";
+            break;
+    }
+
     main.classList.remove("mainMap");
     main.innerHTML = `
     <div>
         <div class="charracterImage"></div>
         <div class="charracterInfo">
             <h2>${charracter}</h2>
-            <p>Relationship to victim</p>
+            <button>Misstänkt</button>
             <button class="btnPlayAudio"></button>
         </div>
     </div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est. Vitae tortor condimentum lacinia quis vel eros donec. Tristique et egestas quis ipsum. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Curabitur vitae nunc sed velit dignissim sodales ut. Risus nullam eget felis eget nunc lobortis mattis aliquam. Venenatis a condimentum vitae sapien pellentesque. Mi eget mauris pharetra et ultrices neque ornare aenean euismod.  </p>
+    <p>${charracterText} </p>
+    <button class="guessMurderer">Mördare</button>
     `;
+
+    main.querySelector(".guessMurderer").addEventListener("click", e => {
+        let confirmPopup = document.createElement("div");
+        confirmPopup.classList.add("popupBackground");
+        confirmPopup.innerHTML = `
+        <div class="confirmPopup">
+            <p>Är du säker på att ${charracter} är mördaren?</p>
+            <div>
+                <button class="btnContinuePlaying">Fortsätt spela</button>
+                <button class="btnGuidedMurderer">Ja</button>
+            </div>
+        </div>
+        `;
+
+        main.appendChild(confirmPopup);
+
+        main.querySelector(".btnContinuePlaying").addEventListener("click", e => {
+            confirmPopup.remove();
+        })
+        main.querySelector(".btnGuidedMurderer").addEventListener("click", e => {
+            console.log("guessing murderer");
+        })
+    })
 }
 
 function renderLeaderboard(){
